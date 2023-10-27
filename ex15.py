50afb010677e2c471b0fe2a18c721f15e3622aa4
luisay11 <147919849+luisay11@users.noreply.github.com>
2023-10-27 13:26:06 +0300

    Add files via upload

---
+++ ex15.py
@@ -0,0 +1,13 @@
+nr_coloana = int(input("Introduceti un numar!"))
+nr_rand_original = int(input("Introduceti un numar!"))
+
+while nr_coloana > 0 :
+    nr_rand = nr_rand_original
+    while nr_rand > 0:
+        nr_rand = nr_rand - 1
+        print("0", end = " ")
+    nr_coloana = nr_coloana - 1
+    print (" ")
+   
+   
+    
